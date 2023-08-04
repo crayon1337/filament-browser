3d3f550f0b6c6a5e3171fdf548db1c1fdc935bbd
Mohamed Mahmoud <mohammedarey2@gmail.com>
2023-08-04 16:53:42 +0300

    Fixing translations

--- resources/views/browser.blade.php
+++ resources/views/browser.blade.php
@@ -2,24 +2,26 @@
     <div class="grid grid-cols-1 gap-1">
         <x-filament::card>
             <div id="app">
-                <browser class="py-4" :collection="{{ json_encode([
-                    "folders" => $folders,
-                    "files" => $files,
-                    "back_path" => $back_path,
-                    "back_name" => $back_name,
-                    "current_path" => $current_path
-                ]) }}" :url="'{{ url('admin/browser') }}'" inline-template v-cloak>
+                <browser class="py-4"
+                    :collection="{{ json_encode([
+                        'folders' => $folders,
+                        'files' => $files,
+                        'back_path' => $back_path,
+                        'back_name' => $back_name,
+                        'current_path' => $current_path,
+                    ]) }}"
+                    :url="'{{ url('admin/browser') }}'" inline-template v-cloak>
                     <div>
                         <div class="flex justify-start py-4 space-x-2">
                             <button @click="goHome()"
                                 class="inline-flex items-center justify-center font-medium tracking-tight transition rounded-lg focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 h-9 px-4 text-white shadow focus:ring-white">
                                 <x-heroicon-s-home class="w-6 h-6 p-1  item-center" />
-                                {{ __('Home') }}
+                                Home
                             </button>
                             <button @click="goBack()"
                                 class="inline-flex items-center justify-center font-medium tracking-tight transition rounded-lg focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 h-9 px-4 text-white shadow focus:ring-white">
                                 <x-heroicon-s-backspace class="w-6 h-6 p-1  item-center" />
-                                {{ __('Back') }}
+                                Back
                             </button>
                         </div>
                         <div v-if="fileContent">
@@ -29,7 +31,7 @@
                             <button @click="saveFile()"
                                 class="inline-flex items-center justify-center font-medium tracking-tight transition rounded-lg focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 h-9 px-4 text-white shadow focus:ring-white">
                                 <x-heroicon-s-save class="w-6 h-6 p-1  item-center" />
-                                {{ __('Save File') }}
+                                Save File
                             </button>
                         </div>
                         <div v-else-if="imagePath">
@@ -42,7 +44,7 @@
                                     <button @click="getFolder(folder)">
                                         <x-heroicon-s-folder class="w-20 h-20  item-center text-primary-500" />
                                         <button
-                                            class="block font-medium mt-4 text-center">@{{ folder.name.substring(0,20) }}</button>
+                                            class="block font-medium mt-4 text-center">@{{ folder.name.substring(0, 20) }}</button>
                                     </button>
                                 </div>
                                 <div class="border rounded flex flex-col items-center p-4"
@@ -53,7 +55,7 @@
                                             class="w-20 h-20  item-center text-primary-500" />
                                         <x-heroicon-o-document-text v-else class="w-20 h-20  item-center" />
                                         <button
-                                            class="block font-medium mt-4 text-center">@{{ file.name.substring(0,20) }}</button>
+                                            class="block font-medium mt-4 text-center">@{{ file.name.substring(0, 20) }}</button>
                                     </button>
                                 </div>
 
@@ -78,5 +80,5 @@
 
 
 @push('scripts')
-<script src="{{ asset('js/filament-browser.js') }}"></script>
+    <script src="{{ asset('js/filament-browser.js') }}"></script>
 @endpush
