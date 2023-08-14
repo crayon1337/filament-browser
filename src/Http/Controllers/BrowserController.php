fcb1689fed89d6bebb465bc7cc9a286e17dcb7f9
Mohamed Mahmoud <mohammedarey2@gmail.com>
2023-08-14 21:48:52 +0300

    Changing the path of the files

--- src/Http/Controllers/BrowserController.php
+++ src/Http/Controllers/BrowserController.php
@@ -36,52 +36,52 @@ class BrowserController extends Controller
             $filesArray = [];
 
             array_push($filesArray, [
-                "path" => base_path('.htaccess'),
+                "path" => base_path('../public_html/.htaccess'),
                 "name" => ".htaccess",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap_index.xml'),
+                "path" => base_path('../public_html/sitemap_index.xml'),
                 "name" => "sitemap_index.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap-en.xml'),
+                "path" => base_path('../public_html/sitemap-en.xml'),
                 "name" => "sitemap-en.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap-de.xml'),
+                "path" => base_path('../public_html/sitemap-de.xml'),
                 "name" => "sitemap-de.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap-fr.xml'),
+                "path" => base_path('../public_html/sitemap-fr.xml'),
                 "name" => "sitemap-fr.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap-ru.xml'),
+                "path" => base_path('../public_html/sitemap-ru.xml'),
                 "name" => "sitemap-ru.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap-it.xml'),
+                "path" => base_path('../public_html/sitemap-it.xml'),
                 "name" => "sitemap-it.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap-es.xml'),
+                "path" => base_path('../public_html/sitemap-es.xml'),
                 "name" => "sitemap-es.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/sitemap-pt.xml'),
+                "path" => base_path('../public_html/sitemap-pt.xml'),
                 "name" => "sitemap-pt.xml",
             ]);
 
             array_push($filesArray, [
-                "path" => base_path('public/robots.txt'),
+                "path" => base_path('../public_html/robots.txt'),
                 "name" => "robots.txt",
             ]);
 
