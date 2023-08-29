c02f7b17ed8245be8ddc2e3b3d0e28c909aeb428
Mohamed Mahmoud <mohammedarey2@gmail.com>
2023-08-29 12:15:47 +0300

    Reverting back sitemap

--- src/Pages/Browser.php
+++ src/Pages/Browser.php
@@ -22,57 +22,21 @@ class Browser extends Page
     protected function getViewData(): array
     {
         $foldersArray = [];
-        $filesArray = [];
 
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap_index.xml'),
-            "name" => "sitemap_index.xml",
-        ]);
+        $filesArray[] = [
+            "path" => base_path('../public_html/sitemap.xml'),
+            "name" => "sitemap.xml",
+        ];
 
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap-en.xml'),
-            "name" => "sitemap-en.xml",
-        ]);
-
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap-de.xml'),
-            "name" => "sitemap-de.xml",
-        ]);
-
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap-fr.xml'),
-            "name" => "sitemap-fr.xml",
-        ]);
-
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap-ru.xml'),
-            "name" => "sitemap-ru.xml",
-        ]);
-
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap-it.xml'),
-            "name" => "sitemap-it.xml",
-        ]);
-
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap-es.xml'),
-            "name" => "sitemap-es.xml",
-        ]);
-
-        array_push($filesArray, [
-            "path" => base_path('../public_html/sitemap-pt.xml'),
-            "name" => "sitemap-pt.xml",
-        ]);
-
-        array_push($filesArray, [
+        $filesArray[] = [
             "path" => base_path('../public_html/.htaccess'),
             "name" => ".htaccess",
-        ]);
+        ];
 
-        array_push($filesArray, [
+        $filesArray[] = [
             "path" => base_path('../public_html/robots.txt'),
             "name" => "robots.txt",
-        ]);
+        ];
 
         return [
             "folders" => $foldersArray,
